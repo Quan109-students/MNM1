7bfbbd58af963257bc3f0ce56ca491fcf293fae9
Quan109-students <huynhphuocquan2005@gmail.com>
2025-03-26 21:15:47 +0700

    Buoi8Buoi8

---
+++ Buoi8/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const rootRouter = require("./routes/root")
+const userRouter = require("./routes/user")
+const bookingRouter = require("./routes/booking")
+const HomeController = require('./controllers/home_controller');
+const connectMongo = require("./config/connectDB");
+connectMongo().catch(err => console.log(err));
+
+const app = express();
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.json())
+
+app.use("/user", userRouter)
+app.use("/", rootRouter)
+app.use("/booking", bookingRouter)
+
+app.use("/", HomeController.index);
+app.get("/about", HomeController.about);
+
+app.get("/", (req, res)=>{
+    res.render("index", {user});
+});
+
+app.listen(3000, ()=>{
+    console.log("Server Started!!!")
+})
